01b886cf5535ad6e09b31c55d3fd47be81314bf2
2611006 <134834192+2611006@users.noreply.github.com>
2026-01-05 18:16:34 +0530

    Update NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,1 +1,10 @@
+const NotFound = () => {
+  return (
+    <div className="flex h-screen items-center justify-center text-xl font-semibold">
+      404 | Page Not Found
+    </div>
+  );
+};
+
+export default NotFound;
 
